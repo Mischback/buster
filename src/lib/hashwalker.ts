d94cb8ed3e51739d71b093626153d20bf600eaaa
Mischback <dev@mischback.de>
2021-11-04 00:05:47 +0100

    Implement createHashedFile()

--- src/lib/hashwalker.ts
+++ src/lib/hashwalker.ts
@@ -3,11 +3,11 @@
 /* library imports */
 import { createHash } from "crypto";
 import { createReadStream } from "fs";
-import { readdir, stat } from "fs/promises";
+import { copyFile as fscopyFile, readdir, rename, stat } from "fs/promises";
 import { basename, dirname, extname, join, resolve as pathresolve } from "path";
 
 /* internal imports */
-import { BusterConfig } from "./configure";
+import { BusterConfig, MODE_COPY, MODE_RENAME } from "./configure";
 import { BusterError } from "./errors";
 import { logger } from "./logging";
 
@@ -25,6 +25,99 @@ class BusterHashWalkerFilterError extends BusterHashWalkerError {
   constructor(message: string) {
     super(message);
   }
+}
+
+/**
+ * Copy a file
+ *
+ * @param source - The original file provided as string
+ * @param destination - The new filename and copy target
+ * @returns - A Promise, resolving to the new filename
+ *
+ * This function is not meant to be called directly, {@link createHashedFile}
+ * wraps around it and determines which mode is to be used.
+ *
+ * @see {@link renameFile}
+ */
+function copyFile(source: string, destination: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fscopyFile(source, destination)
+      .then(() => {
+        return resolve(destination);
+      })
+      .catch((err) => {
+        logger.debug(err);
+        return reject(new BusterHashWalkerError("Could not copy file"));
+      });
+  });
+}
+
+/**
+ * Rename a file
+ *
+ * @param source - The original file provided as string
+ * @param destination - The new filename
+ * @returns - A Promise, resolving to the new filename
+ *
+ * This function is not meant to be called directly, {@link createHashedFile}
+ * wraps around it and determines which mode is to be used.
+ *
+ * @see {@link copyFile}
+ */
+function renameFile(source: string, destination: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    rename(source, destination)
+      .then(() => {
+        return resolve(destination);
+      })
+      .catch((err) => {
+        logger.debug(err);
+        return reject(new BusterHashWalkerError("Could not rename file"));
+      });
+  });
+}
+
+/**
+ * Create the file with its content hash appended to the filename
+ *
+ * @param source - The original source file
+ * @param destination - The new filename as returned by {@link determineNewFilename}
+ * @param mode - Operation mode, either {@link MODE_COPY} or {@link MODE_RENAME}
+ * @returns - A Promise, resolving to the new filename
+ *
+ * Buster provides two different modes of operation:
+ * - in copy mode, the source file is copied with destination as its new filename
+ * - in rename mode, the source file is renamed to destination
+ *
+ * The actual filesystem operations are performed by {@link copyFile} and
+ * {@link renameFile}.
+ */
+function createHashedFile(
+  source: string,
+  destination: string,
+  mode: string
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let fileFunc;
+    switch (mode) {
+      case MODE_COPY:
+        fileFunc = copyFile;
+        break;
+      case MODE_RENAME:
+        fileFunc = renameFile;
+        break;
+      default:
+        return reject(new BusterHashWalkerError("Unknown mode"));
+    }
+
+    fileFunc(source, destination)
+      .then(() => {
+        return resolve(destination);
+      })
+      .catch((err) => {
+        return reject(err);
+      });
+  });
 }
 
 /**
@@ -174,7 +267,10 @@ export function hashWalker(
                     return determineNewFilename(file, hash, config.hashLength);
                   })
                   .then((newFilename) => {
-                    logger.debug(newFilename);
+                    return createHashedFile(file, newFilename, config.mode);
+                  })
+                  .then((newFilename) => {
+                    logger.debug(`"${file}" => "${newFilename}"`);
                   })
                   .catch((err) => {
                     if (!(err instanceof BusterHashWalkerFilterError))
