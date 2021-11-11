d362ce739d1ac5ff0068a8376bb178e485e34e13
Mischback <dev@mischback.de>
2021-11-11 20:56:25 +0100

    Implement load/write JSON

---
+++ src/lib/json-interface.ts
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+/* library imports */
+import { readFile, writeFile } from "fs/promises";
+
+export class JSONInterfaceError extends Error {
+  originalError: Error;
+
+  constructor(message: string, err: Error) {
+    super(message);
+    this.originalError = err;
+  }
+}
+
+/**
+ * Read a given file and parse its content as JSON
+ *
+ * @param filename - The file to be read provided as string
+ * @returns A Promise, resolving to the file content as JSON
+ *
+ * The returned JSON object will be of type "unknown" and must be casted to
+ * the required target type.
+ */
+export function loadJsonFromFile(filename: string): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    readFile(filename, "utf-8")
+      .then((fileContent) => {
+        return resolve(JSON.parse(fileContent));
+      })
+      .catch((err: Error) => {
+        return reject(
+          new JSONInterfaceError(`Could not read "${filename}"`, err)
+        );
+      });
+  });
+}
+
+/**
+ * Write a given object to a file in JSON
+ *
+ * @param filename - The file to be written to
+ * @param jsonData - The object to be written to the file
+ * @returns A Promise, resolving to 0 of type JsonInterfaceRetValSuccess
+ */
+export function writeJsonToFile(
+  filename: string,
+  jsonData: unknown
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    writeFile(filename, JSON.stringify(jsonData))
+      .then(() => {
+        return resolve();
+      })
+      .catch((err: Error) => {
+        return reject(
+          new JSONInterfaceError(`Could not write to file "${filename}"`, err)
+        );
+      });
+  });
+}
